6cd79490fb956bc3283382c0eefc6a89ebe79a9e
Alexa Kayman <alexakayman@gmail.com>
2025-03-09 19:41:03 -0400

    opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,16 @@ export const metadata: Metadata = {
   title: "Save Research | Columbia University",
   description:
     "Our response to the $400 million budget cuts. Helping researchers in need. Built by Alexa Kayman, Alex Denuzzo, and Joseph JoJoe.",
+  openGraph: {
+    images: [
+      {
+        url: "/graphimage.png",
+        width: 1200,
+        height: 630,
+        alt: "Save Research | Columbia University",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +25,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <footer className="text-center p-4 mb-4">
+        Built by{" "}
+        <a
+          href="https://alexakayman.com"
+          className="text-blue-500 hover:underline"
+        >
+          Alexa Kayman
+        </a>
+        .<br></br>Co-conspired by{" "}
+        <a
+          href="https://www.linkedin.com/in/josephjojo/"
+          className="text-blue-500 hover:underline"
+        >
+          Joseph JoJoe
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.linkedin.com/in/alexdenuzzo/"
+          className="text-blue-500 hover:underline"
+        >
+          Alex Denuzzo
+        </a>
+        , who published this{" "}
+        <a
+          href="https://www.linkedin.com/in/alexdenuzzo/"
+          className="text-blue-500 hover:underline"
+        >
+          letter
+        </a>
+        .
+      </footer>
     </html>
   );
 }
