631168a794a2432643af3487e6a085315ecc562d
Alexa Kayman <alexakayman@gmail.com>
2025-03-10 23:46:12 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Save Research | Columbia University",
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
